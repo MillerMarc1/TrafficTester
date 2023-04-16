7024c1703ebe81d5cf69f1e417b139e939eb4bd9
Joshua Restuccia <joshua.restuccia@gmail.comm>
2023-04-16 12:27:43 -0400

    Added all pages

--- traffic-tester/client/src/App.js
+++ traffic-tester/client/src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Graph from "./pages/Graph";
+import Edit from "./pages/Edit";
+import Save from "./pages/Save";
+import Load from "./pages/Load";
 
 import "./App.css";
 
@@ -7,7 +12,11 @@ export const App = () => {
   return (
     <div className="App">
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/graph" element={<Graph />} />
+        <Route path="/edit" element={<Edit />} />
+        <Route path="/save" element={<Save />} />
+        <Route path="/load" element={<Load />} />
       </Routes>
     </div>
   );
