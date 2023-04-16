49a955100e6f4bd43a216823a2520a5376e16033
Joshua Restuccia <joshua.restuccia@gmail.comm>
2023-04-16 12:22:37 -0400

    Added react-router and new pages

--- traffic-tester/client/src/App.js
+++ traffic-tester/client/src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
-function App() {
+import "./App.css";
+
+export const App = () => {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
